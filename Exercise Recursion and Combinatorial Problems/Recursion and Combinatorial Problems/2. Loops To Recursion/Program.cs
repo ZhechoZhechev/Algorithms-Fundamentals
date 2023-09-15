8613ad31468ac4dc97933e59dc658b6aa90faee7
ZhechoZhechev <mishomishev024@gmail.com>
2023-09-15 17:49:13 +0300

    Recursion and Combinatorial Problems Exercise

---
+++ Exercise Recursion and Combinatorial Problems/Recursion and Combinatorial Problems/2. Loops To Recursion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _2._Loops_To_Recursion
+{
+    public class Program
+    {
+        private static string[] combinations;
+
+        static void Main()
+        {
+            var nestedNumber = int.Parse(Console.ReadLine()!);
+            combinations = new string[nestedNumber];
+
+            GetCombination(0);
+        }
+
+        private static void GetCombination(int index)
+        {
+            if (index >= combinations.Length)
+            {
+                Console.WriteLine(string.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = 1; i <= combinations.Length; i++)
+            {
+                combinations[index] = i.ToString();
+                GetCombination(index + 1);
+            }
+        }
+    }
+}
