12482ccb4b2ebf98b4ad846324a2c5cdb73fe43b
ZhechoZhechev <mishomishev024@gmail.com>
2023-12-26 15:55:50 +0200

    Some Interesting Problems

---
+++ Exercise Recursion and Combinatorial Problems/TechnicalInterviewProblems/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace UpsAndDowns;
+
+internal class Program
+{
+    private static int numebеrWeSearch;
+
+    static void Main()
+    {
+        numebеrWeSearch = 13;
+
+        Console.WriteLine(FindTheNumber(1, 100));
+    }
+
+    private static int FindTheNumber(int left, int right)
+    {
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            if (Guess(mid) == 0)
+            {
+                return mid;
+            }
+            else if (Guess(mid) == 1)
+            {
+                left = mid + 1;
+            }
+            else if (Guess(mid) == -1)
+            {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    private static int Guess(int number)
+    {
+        if (number == numebеrWeSearch) return 0;
+        else if (number < numebеrWeSearch) return 1;
+        else return -1;
+    }
+}
