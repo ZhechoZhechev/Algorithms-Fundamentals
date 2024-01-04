6ac036b77e8a755001beb9a7bff15ffdb3600e85
ZhechoZhechev <mishomishev024@gmail.com>
2024-01-04 16:44:58 +0200

    Lab Graphs

---
+++ Lab Graph Theory, Traversal, and Shortest Paths/GraphTheoryTraversalandShortestPaths/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace GraphTheoryTraversalandShortestPaths;
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+internal class Program
+{
+    private static List<int>[] graph;
+    private static bool[] visited;
+    static void Main(string[] args)
+    {
+        var n = int.Parse(Console.ReadLine());
+
+        graph = new List<int>[n];
+        visited = new bool[n];
+
+        for (int node = 0; node < n; node++)
+        {
+            var line = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(line))
+            {
+                graph[node] = new List<int>();
+            }
+            else
+            {
+                var children = line.Split()
+                    .Select(int.Parse)
+                    .ToList();
+
+                graph[node] = children;
+            }
+        }
+
+        for (int node = 0; node < graph.Length; node++)
+        {
+            if (visited[node])
+            {
+                continue;
+            }
+
+            var component = new List<int>();
+            DFS(node, component);
+
+            Console.WriteLine($"Connected component: {string.Join(" ", component)}");
+        }
+    }
+
+    private static void DFS(int node, List<int> component)
+    {
+        if (visited[node])
+        {
+            return;
+        }
+
+        visited[node] = true;
+
+        foreach (var child in graph[node])
+        {
+            DFS(child, component);
+        }
+
+        component.Add(node);
+    }
+}
