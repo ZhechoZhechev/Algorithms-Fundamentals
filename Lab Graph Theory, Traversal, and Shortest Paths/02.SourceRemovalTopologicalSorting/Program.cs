c0aebfd576e91f5ba94af91e424d06991aeb5980
ZhechoZhechev <mishomishev024@gmail.com>
2024-01-05 18:59:14 +0200

    Graphs Lab

---
+++ Lab Graph Theory, Traversal, and Shortest Paths/02.SourceRemovalTopologicalSorting/Program.cs
@@ -0,0 +1,104 @@
+﻿namespace _02.SourceRemovalTopologicalSorting;
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+internal class Program
+{
+    private static Dictionary<string, List<string>> graph;
+    private static Dictionary<string, int> dependencies;
+    static void Main()
+    {
+        int nodesCount = int.Parse(Console.ReadLine());
+
+        graph = GraphRead(nodesCount);
+
+        dependencies = ExtractDependencies(graph);
+
+        var isLoop = false;
+        List<string> sorted = new List<string>();
+        while (dependencies.Count > 0)
+        {
+            var nodeToRemove = dependencies.FirstOrDefault(x => x.Value == 0).Key;
+
+            if (nodeToRemove == null)
+            {
+                isLoop = true;
+                break;
+            }
+
+            dependencies.Remove(nodeToRemove);
+            sorted.Add(nodeToRemove);
+
+            foreach (var child in graph[nodeToRemove])
+            {
+                dependencies[child] -= 1;
+            }
+        }
+        if (isLoop)
+        {
+            Console.WriteLine("Invalid topological sorting");
+        }
+        else
+        {
+            Console.WriteLine($"Topological sorting: {string.Join(", ", sorted)}");
+        }
+    }
+
+    private static Dictionary<string, int> ExtractDependencies(Dictionary<string, List<string>> graph)
+    {
+        var result = new Dictionary<string, int>();
+
+        foreach (var kvp in graph)
+        {
+            var node = kvp.Key;
+            var children = kvp.Value;
+
+            if (!result.ContainsKey(node))
+            {
+                result[node] = 0;
+            }
+
+            foreach (var child in children)
+            {
+                if (!result.ContainsKey(child))
+                {
+                    result[child] = 1;
+                }
+                else
+                {
+                    result[child] += 1;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static Dictionary<string, List<string>> GraphRead(int nodesCount)
+    {
+        var result = new Dictionary<string, List<string>>();
+
+        for (int i = 0; i < nodesCount; i++)
+        {
+            var parts = Console.ReadLine()
+                .Split("->", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim())
+                .ToArray();
+
+            var node = parts[0];
+            if (parts.Length == 1)
+            {
+                result[node] = new List<string>();
+            }
+            else
+            {
+                var children = parts[1].Split(", ").ToList();
+                result[node] = children;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ Lab Graph Theory, Traversal, and Shortest Paths/3.ShortestPath/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace _03.ShortestPath
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    internal class Program
+    {
+        private static List<int>[] graph;
+        private static bool[] visited;
+        private static int[] parent;
+        static void Main()
+        {
+            var nodesCount = int.Parse(Console.ReadLine()!);
+            var edgesCount = int.Parse(Console.ReadLine()!);
+
+            graph = new List<int>[nodesCount + 1];
+            visited = new bool[graph.Length];
+
+            parent = new int[graph.Length];
+            Array.Fill(parent, -1);
+
+            for (int i = 0; i < graph.Length; i++)
+            {
+                graph[i] = new List<int>();
+            }
+
+            for (int i = 0; i < edgesCount; i++)
+            {
+                var edge = Console.ReadLine()
+                    .Split()
+                    .Select(x => int.Parse(x))
+                    .ToArray();
+
+                var firstNode = edge[0];
+                var secondNode = edge[1];
+
+                graph[firstNode].Add(secondNode);
+                graph[secondNode].Add(firstNode);
+            }
+
+            var start = int.Parse(Console.ReadLine()!);
+            var end = int.Parse(Console.ReadLine()!);
+
+            BFS(start, end);
+        }
+
+        private static void BFS(int startNode, int destination)
+        {
+            var queue = new Queue<int>();
+            queue.Enqueue(startNode);
+
+            visited[startNode] = true;
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+
+                if (node == destination)
+                {
+                    var path = GetPath(destination);
+                    path.Reverse();
+
+                    Console.WriteLine($"Shortest path length is: {path.Count - 1}");
+                    Console.WriteLine($"{string.Join(" ",path)}");
+
+                    break;
+                }
+
+                foreach (var child in graph[node])
+                {
+                    if (!visited[child])
+                    {
+                        parent[child] = node;
+                        queue.Enqueue(child);
+                        visited[child] = true;
+                    }
+                }
+            }
+        }
+
+        private static List<int> GetPath(int destination)
+        {
+            var path = new List<int>();
+
+            var idx = destination;
+
+            while (idx != -1)
+            {
+                path.Add(idx);
+                idx = parent[idx];
+            }
+
+            return path;
+        }
+    }
+}
