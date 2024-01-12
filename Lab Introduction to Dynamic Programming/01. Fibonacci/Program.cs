3f5d2130e4540e592a3724f5eb4f44bea3989b14
ZhechoZhechev <mishomishev024@gmail.com>
2024-01-12 17:19:56 +0200

    Dynamic Programming

---
+++ Lab Introduction to Dynamic Programming/01. Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _01._Fibonacci
+{
+    using System;
+    using System.Collections.Generic;
+    internal class Program
+    {
+        private static Dictionary<int, long> fibonaccies = new Dictionary<int, long>();
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            long result = CalculateFibonacci(num);
+            Console.WriteLine(result);
+        }
+
+        private static long CalculateFibonacci(int num)
+        {
+
+            if (fibonaccies.ContainsKey(num))
+            {
+                return fibonaccies[num];
+            }
+
+            if (num < 3)
+            {
+                return 1;
+            }
+
+            var result = CalculateFibonacci(num - 1) + CalculateFibonacci(num - 2);
+            fibonaccies[num] = result;
+            return result;
+        }
+    }
+}
---
+++ Lab Introduction to Dynamic Programming/02.Move Down_Right/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace _02.Move_Down_Right
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine()!);
+            int cols = int.Parse(Console.ReadLine()!);
+
+            int[,] matrix = new int[rows, cols];
+            int[,] sums = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                var input = Console.ReadLine()!
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int j = 0; j < input.Length; j++)
+                {
+                    matrix[i, j] = input[j];
+                }
+            }
+
+            sums[0, 0] = matrix[0, 0];
+
+            for (int i = 1; i < rows; i++)
+            {
+                var sum = sums[0, i - 1] + matrix[0, i];
+                sums[0, i] = sum;
+            }
+
+            for (int i = 1; i < cols; i++)
+            {
+                var sum = sums[i - 1, 0] + matrix[i, 0];
+                sums[i, 0] = sum;
+            }
+
+            for (int row = 1; row < rows; row++)
+            {
+                for (int col = 1; col < cols; col++)
+                {
+                    var sum = Math.Max(sums[row - 1, col], sums[row, col - 1]) + matrix[row, col];
+                    sums[row, col] = sum;
+                }
+            }
+
+            var path = new Stack<string>();
+            var curRow = rows - 1;
+            var curCol = cols - 1;
+            while (curRow > 0 && curCol > 0)
+            {
+                path.Push($"[{curRow}, {curCol}]");
+                var up = sums[curRow - 1, curCol];
+                var left = sums[curRow, curCol - 1];
+
+                if (up > left)
+                {
+                    curRow -= 1;
+
+                }
+                else
+                {
+                    curCol -= 1;
+
+                }
+            }
+
+            while (curRow > 0) 
+            {
+                path.Push($"[{curRow}, {curCol}]");
+                curRow -= 1;
+
+            }
+
+            while (curCol > 0)
+            {
+                path.Push($"[{curRow}, {curCol}]");
+                curCol -= 1;
+
+            }
+
+            path.Push($"[{curRow}, {curCol}]");
+            Console.WriteLine(string.Join(" ", path));
+        }
+    }
+}
